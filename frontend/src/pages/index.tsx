f84bd4e8e077f5dec15bf44fa5d6d7fc5724c0c6
Antti Pessa <anttipessa@gmail.com>
2023-03-07 23:43:09 +0200

    Update Readme

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,25 +1,11 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
 export default function Home() {
-
-  // Give session DefaultSession type
-  interface DefaultSession extends Record<string, unknown> {
-    user?: {
-      name?: string | null
-      email?: string | null
-      image?: string | null
-    }
-    expires?: string
-  }
-  
-
   const { data: session, status: loading } = useSession();
-
-  //f (loading) return null;
 
   if (session) {
     return (
       <div>
-        Signed in as {session.user.email} <br />
+        Signed in as {session.user?.email || session.user?.name} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </div>
     );
