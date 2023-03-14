6fb6d0a59afa91bd112a7f8172a9153536e466ed
Antti Pessa <anttipessa@gmail.com>
2023-03-14 16:44:31 +0200

    Inital terraform setup

---
+++ terraform/s3.tf
@@ -0,0 +1,44 @@
+resource "aws_s3_bucket" "static_react_bucket" {
+  bucket = "react-bucket-hosted-aws-s3-terraform"
+  tags = {
+    Name = "react-bucket"
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "s3_acl" {
+  bucket = aws_s3_bucket.static_react_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_s3" {
+  bucket = aws_s3_bucket.static_react_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket                  = aws_s3_bucket.static_react_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "react_app_s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.static_react_bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.oai.iam_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "react_app_bucket_policy" {
+  bucket = aws_s3_bucket.static_react_bucket.id
+  policy = data.aws_iam_policy_document.react_app_s3_policy.json
+}
