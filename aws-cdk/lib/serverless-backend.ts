e962e4c568af2a29b7d11ee8ab60bfb125c31aa2
Antti Pessa <anttipessa@gmail.com>
2023-04-06 15:22:05 +0300

    Add logging for lambda and apigw

--- aws-cdk/lib/serverless-backend.ts
+++ aws-cdk/lib/serverless-backend.ts
@@ -9,6 +9,9 @@ import {
   HttpMethod,
 } from "@aws-cdk/aws-apigatewayv2-alpha";
 import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
+import { PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { CfnStage } from "aws-cdk-lib/aws-apigatewayv2";
 
 export class ServerlessBackend extends Construct {
   constructor(parent: Stack, name: string) {
@@ -25,11 +28,13 @@ export class ServerlessBackend extends Construct {
     const readFunction = new NodejsFunction(this, "ReadMsgsFn", {
       architecture: Architecture.ARM_64,
       entry: `./../lambda/readFunction.ts`,
+      logRetention: RetentionDays.ONE_DAY,
     });
 
     const writeFunction = new NodejsFunction(this, "WriteMsgsFn", {
       architecture: Architecture.ARM_64,
       entry: `./../lambda/writeFunction.ts`,
+      logRetention: RetentionDays.ONE_DAY,
     });
 
     table.grantReadData(readFunction);
@@ -43,6 +48,49 @@ export class ServerlessBackend extends Construct {
         allowOrigins: ["*"],
       },
     });
+
+    const accessLogs = new LogGroup(this, 'APIGW-AccessLogs', {
+      logGroupName: 'APIGW-AccessLogs',
+      retention: RetentionDays.ONE_DAY,
+    })
+
+    const stage = api.defaultStage?.node.defaultChild as CfnStage
+    stage.accessLogSettings = {
+      destinationArn: accessLogs.logGroupArn,
+      format: JSON.stringify({
+        requestId: '$context.requestId',
+        userAgent: '$context.identity.userAgent',
+        sourceIp: '$context.identity.sourceIp',
+        requestTime: '$context.requestTime',
+        requestTimeEpoch: '$context.requestTimeEpoch',
+        httpMethod: '$context.httpMethod',
+        path: '$context.path',
+        status: '$context.status',
+        protocol: '$context.protocol',
+        responseLength: '$context.responseLength',
+        domainName: '$context.domainName'
+      })
+    }
+
+    const role = new Role(this, 'ApiGWLogWriterRole', {
+      assumedBy: new ServicePrincipal('apigateway.amazonaws.com')
+    })
+
+    const policy = new PolicyStatement({
+      actions: [
+        'logs:CreateLogGroup',
+        'logs:CreateLogStream',
+        'logs:DescribeLogGroups',
+        'logs:DescribeLogStreams',
+        'logs:PutLogEvents',
+        'logs:GetLogEvents',
+        'logs:FilterLogEvents'
+      ],
+      resources: ['*']
+    })
+
+    role.addToPolicy(policy)
+    accessLogs.grantWrite(role)
 
     const readIntegration = new HttpLambdaIntegration(
       "ReadIntegration",
